834b0b9002386b66dd3d83130a2dfa0a745a2950
Andy Fang <andy@boringordinary.com>
2022-09-17 17:18:13 -0400

    fade nav

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,13 @@
 import cx from "clsx";
 
 interface Props {
+  className?: string;
   size?: "xs" | "sm" | "md" | "lg" | "xl";
   children?: React.ReactNode;
   align?: "left" | "center" | "right";
 }
 
-const Section = ({ size = "md", children, align }: Props) => {
+const Section = ({ size = "md", children, align, className }: Props) => {
   const styles = cx({
     "w-full max-w-screen-md md:max-w-screen-lg xl:max-w-screen-xl mx-auto px-4":
       true,
@@ -16,6 +17,7 @@ const Section = ({ size = "md", children, align }: Props) => {
     "py-12": size === "lg",
     "py-16": size === "xl",
     "flex justify-center": align === "center",
+    [className as string]: className,
   });
 
   return <div className={styles}>{children}</div>;
